b0909225da1c777569cee7f18b8f05c2b5168084
ida ms <119769726+idamsufiana@users.noreply.github.com>
2023-08-18 05:15:02 +0700

    Add files via upload

---
+++ IceCreamMachine.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IceCreamMachine {
+    public String[] ingredients;
+    public String[] toppings;
+    
+    public static class IceCream {
+        public String ingredient;
+        public String topping;
+
+        public IceCream(String ingredient, String topping) {
+            this.ingredient = ingredient;
+            this.topping = topping;
+        }
+    }
+
+    public IceCreamMachine(String[] ingredeints, String[] toppings) {
+        this.ingredients = ingredeints;
+        this.toppings = toppings;
+    }
+
+    public List<IceCream> scoops() {
+
+        List<IceCream> scoops = new ArrayList<>();
+        
+        for (int j = 0; j<ingredients.length;j++){
+            for(int l = 0; l<toppings.length;l++){
+                scoops.add(new IceCream(ingredients[j],toppings[l]));
+            }
+        }
+        return scoops;
+    }
+
+    public static void main(String[] args) {
+        IceCreamMachine machine = new IceCreamMachine(new String[]{
+                "vanilla", "chocolate"
+        }, new String[]{
+                "chocolate sauce"
+        });
+        List<IceCream> scoops = machine.scoops();
+
+        /*
+         * Should print:
+         * vanilla, chocolate sauce
+         * chocolate, chocolate sauce
+         */
+        for (IceCream iceCream : scoops) {
+            System.out.println(iceCream.ingredient + ", " + iceCream.topping);
+        }
+    }
+}
