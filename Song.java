12cca06c0b966bfd171f44cad288b482dcd0945e
ida ms <119769726+idamsufiana@users.noreply.github.com>
2023-08-18 05:17:53 +0700

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Song {
+    private String name;
+    private Song nextSong;
+
+    public Song(String name) {
+        this.name = name;
+    }
+
+    public void setNextSong(Song nextSong) {
+        this.nextSong = nextSong;
+    }
+
+    public boolean isInRepeatingPlaylist() {
+        //throw new UnsupportedOperationException("Waiting to be implemented.");
+        Song slow = this.nextSong;
+        Song fast = slow == null ? null : slow.nextSong;
+        while (fast != null) {
+            if (slow == this || slow == fast)
+                return true;
+            slow = slow.nextSong;
+            fast = fast.nextSong;
+            if (fast != null)
+                fast = fast.nextSong;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Song first = new Song("Hello");
+        Song second = new Song("Eye of the tiger");
+
+        first.setNextSong(second);
+        second.setNextSong(first);
+
+        System.out.println(first.isInRepeatingPlaylist());
+    }
+}
